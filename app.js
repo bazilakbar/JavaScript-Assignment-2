8bd70a6192abe996b5ee27540e8e142c08e401e5
bazilakbar <bazilakbar4@gmail.com>
2024-04-03 00:09:00 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+let userName = "Muhammad Ali"
+
+let myName = "Roushaan Ahmed"
+
+let message = "Hello World"
+
+alert (message)
+
+let studentName = "Johne Doe"
+
+let studentAge = "15 years old"
+
+studentEnrolledCourse = "Certified Mobile Application Development"
+
+alert (studentName)
+
+alert (studentAge)
+
+alert (studentEnrolledCourse)
+
+let email = "abc@gmail.com"
+
+alert ("My email address is "+email)
+
+let book = "A smarter way to learn JavaScript"
+
+alert ("I am trying to  learn from the Book " +book)
+
+document.write("Yah! I can write HTML content through JavaScript")
